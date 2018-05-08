cc22472777120cbc3416582a32dc34509cdc51bb
Marattty <rosasdeangel@outlook.com>
2018-05-08 13:18:24 -0700

    realagent

---
+++ Desktop/realagent/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AgentsComponent } from './agents/agents.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AgentsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
